7cd049b4af4eefbce0a862b47f38a2bade0cbae1
Caxivitual <nnhuyhoangrp2528@gmail.com>
2024-10-12 09:54:55 +0700

    login + register with google account

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -63,7 +63,7 @@
                 <div class="wave"></div>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="z-0 w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
