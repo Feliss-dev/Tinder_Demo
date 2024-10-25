8a7d2d02557bc11c7dbc55a94e251851269291dc
Caxivitual <nnhuyhoangrp2528@gmail.com>
2024-10-25 08:48:07 +0700

    Refactor

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -23,6 +23,10 @@ new class extends Component
             <div class="flex justify-between">
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <a href="/" class="inline-flex items-center">
+                        <x-logo width="50px" height="50px" />
+                    </a>
+
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
