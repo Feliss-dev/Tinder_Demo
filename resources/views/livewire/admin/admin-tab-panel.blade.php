e91a9888bf1da88c160c0bc99d8263d163b24c72
Caxivitual <nnhuyhoangrp2528@gmail.com>
2024-10-27 12:11:54 +0700

    tab panel

--- resources/views/livewire/admin/admin-tab-panel.blade.php
+++ resources/views/livewire/admin/admin-tab-panel.blade.php
@@ -1,18 +1,58 @@
-<div class="w-full h-full p-2 overflow-auto">
+<div class="w-full h-full p-3 overflow-auto">
     <div x-show="selectedTab === 'management_users'">
-        Management -> Users
+        <h1 class="font-bold text-3xl">User Management</h1>
+        <p class="text-gray-800 mt-2">Manage user registering information.</p>
+
+        <div class="mt-6 flex flex-row justify-between items-center">
+            <h1 class="font-medium text-xl">All users <span class="ml-2 text-gray-600 text-2xl">{{ \App\Models\User::count() }}</span> </h1>
+
+            <div class="flex">
+                <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-white border border-e-0 border-gray-500 rounded-s-md">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                    </svg>
+                </span>
+
+                <input type="text" placeholder="Search by name" class="rounded-none rounded-e-lg border-gray-500" />
+            </div>
+        </div>
+
+        // TODO: User table
+
+{{--        <table class="table-fixed w-full">--}}
+{{--            <thead>--}}
+{{--                <tr>--}}
+{{--                    <th class="text-left w-auto">ID</th>--}}
+{{--                    <th class="text-left">Username</th>--}}
+{{--                    <th class="text-left">Role</th>--}}
+{{--                    <th class="text-left">Actions</th>--}}
+{{--                </tr>--}}
+{{--            </thead>--}}
+
+{{--            <tbody>--}}
+{{--                <tr>--}}
+{{--                    <td>0</td>--}}
+{{--                    <td>A</td>--}}
+{{--                    <td>B</td>--}}
+{{--                    <td>C</td>--}}
+{{--                </tr>--}}
+{{--            </tbody>--}}
+{{--        </table>--}}
     </div>
 
     <div x-show="selectedTab === 'management_conversations'">
-        Management -> Conversations
+        <h1 class="font-bold text-3xl">Conversation Management</h1>
+        <p class="text-gray-800 mt-2">Manage conversation between user and user.</p>
     </div>
 
     <div x-show="selectedTab === 'management_matches'">
-        Management -> Matches
+        <h1 class="font-bold text-3xl">Matching Management</h1>
+        <p class="text-gray-800 mt-2">Manage matching information.</p>
     </div>
 
     <div x-show="selectedTab === 'management_reports'">
-        Management -> Reports
+        <h1 class="font-bold text-3xl">Report Management</h1>
+        <p class="text-gray-800 mt-2">Manage report information.</p>
     </div>
 
     <div x-show="selectedTab === 'application_analytics'">
