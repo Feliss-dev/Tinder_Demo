ffdda97b590c0a085e14f251f10b6ab046a793ef
Caxivitual <nnhuyhoangrp2528@gmail.com>
2024-11-05 15:14:28 +0700

    channels.php: convert ids to integer

--- routes/channels.php
+++ routes/channels.php
@@ -5,11 +5,11 @@ use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Log;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === $id;
+    return $user->id === (int)$id;
 });
 
 Broadcast::channel('users.{id}', function ($user, $id) {
-    return (int) $user->id === $id;
+    return $user->id === (int)$id;
 });
 
 Broadcast::channel('notifications.{id}', function ($user, $id) {
