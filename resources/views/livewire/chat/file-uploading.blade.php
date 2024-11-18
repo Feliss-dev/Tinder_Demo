8b59915b3b18655d9b3c335f90a3a11315ca8762
Caxivitual <nnhuyhoangrp2528@gmail.com>
2024-11-18 15:12:47 +0700

    basic file UI

--- resources/views/livewire/chat/file-uploading.blade.php
+++ resources/views/livewire/chat/file-uploading.blade.php
@@ -1,7 +1,10 @@
-<div @class([ "h-48 border-2 border-red-600" => count($files) > 0 ])>
-    <div class="h-full flex flex-row">
+{{--<div @class([ "border-2 border-red-600 h-48", "hidden" => count($files) == 0 ])>--}}
+<div class="border-2 border-red-600 h-48">
+    <div class="flex flex-row max-w-full h-full overflow-x-auto overflow-y-hidden whitespace-nowrap p-1">
         @foreach ($files as $file)
-            <p>{{$file->getClientOriginalName()}}</p>
+            <div class="h-full aspect-square">
+                <p>{{$file->getClientOriginalName()}}</p>
+            </div>
         @endforeach
     </div>
 </div>
